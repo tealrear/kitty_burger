3b94e07354fa884427c8a58440415f30716238db
kim-revolution-web <kimgiminv@gmail.com>
2026-03-12 17:42:15 +0900

    gui.ui  변경

--- src/sdr_monitoring_station/sdr_monitoring_station/gui_ui.py
+++ src/sdr_monitoring_station/sdr_monitoring_station/gui_ui.py
@@ -31,7 +31,7 @@ class Ui_Form(object):
 "    font-size: 10pt;\n"
 "}\n"
 "\n"
-"QWidget#page, QWidget#page_2, QWidget#page_3 {\n"
+"QWidget#page_0,QWidget#page_1, QWidget#page_2, QWidget#page_3 {\n"
 "    background-color: #1a212c;\n"
 "    border: 1px solid #2c3947;\n"
 "    border-radius: 24px;\n"
@@ -48,7 +48,7 @@ class Ui_Form(object):
 "    border: none;\n"
 "}\n"
 "\n"
-"QLabel#label_page, QLabel#label_page_2, QLabel#label_page_3 {\n"
+"QLabel#label_page_0, QLabel#label_page_1, QLabel#label_page_2, QLabel#label_page_3 {\n"
 "    color: #8ffcff;\n"
 "    font-size: 18pt;\n"
 "    font-weight: bold;\n"
@@ -66,10 +66,10 @@ class Ui_Form(object):
 "QGroupBox {\n"
 "    background-color: #212a35;\n"
 "    border: 2px solid #9adf22;\n"
-"    border-radius: 18px;\n"
+"    border-"
+                        "radius: 18px;\n"
 "    margin-top: 12px;\n"
-""
-                        "    color: #8ffcff;\n"
+"    color: #8ffcff;\n"
 "    font-weight: bold;\n"
 "}\n"
 "\n"
@@ -80,7 +80,8 @@ class Ui_Form(object):
 "    color: #ffb020;\n"
 "}\n"
 "\n"
-"QGroupBox#groupBox_page,\n"
+"QGroupBox#groupBox_page_0,\n"
+"QGroupBox#groupBox_page_1,\n"
 "QGroupBox#groupBox_page_2,\n"
 "QGroupBox#groupBox_page_3 {\n"
 "    background-color: #212a35;\n"
@@ -109,10 +110,10 @@ class Ui_Form(object):
 "\n"
 "QGroupBox#groupBox_go:hover,\n"
 "QGroupBox#groupBox_stop:hover,\n"
-"QGroupBox#groupBox_right:hover,\n"
+""
+                        "QGroupBox#groupBox_right:hover,\n"
 "QGroupBox#groupBox_left:hover,\n"
-"QGroup"
-                        "Box#groupBox_back:hover {\n"
+"QGroupBox#groupBox_back:hover {\n"
 "    background-color: #293544;\n"
 "    border: 2px solid #8ffcff;\n"
 "}\n"
@@ -154,10 +155,10 @@ class Ui_Form(object):
 "    font-weight: bold;\n"
 "}\n"
 "\n"
-"QPushButton:hover {\n"
+""
+                        "QPushButton:hover {\n"
 "    background-color: #2d3b4c;\n"
-"    border: 2px s"
-                        "olid #ffb020;\n"
+"    border: 2px solid #ffb020;\n"
 "}\n"
 "\n"
 "QPushButton:pressed {\n"
@@ -196,12 +197,12 @@ class Ui_Form(object):
 "    border-radius: 18px;\n"
 "    color: #ffb020;\n"
 "    font-size: 22pt;\n"
-"    font-weight: bold;\n"
+"    font-w"
+                        "eight: bold;\n"
 "}\n"
 "\n"
 "QPushButton#btn_face {\n"
-"    background-color:"
-                        " #263240;\n"
+"    background-color: #263240;\n"
 "    border: 2px solid #9adf22;\n"
 "    border-radius: 18px;\n"
 "    color: #ffb020;\n"
@@ -239,10 +240,10 @@ class Ui_Form(object):
 "QTabBar::tab {\n"
 "    background-color: #263240;\n"
 "    color: #8ffcff;\n"
-"    border: 2px solid #9adf22;\n"
+"    bor"
+                        "der: 2px solid #9adf22;\n"
 "    border-top-left-radius: 12px;\n"
-"    border"
-                        "-top-right-radius: 12px;\n"
+"    border-top-right-radius: 12px;\n"
 "    padding: 8px 18px;\n"
 "    margin-right: 4px;\n"
 "    min-width: 90px;\n"
@@ -271,29 +272,48 @@ class Ui_Form(object):
         self.stackedWidget.setObjectName(u"stackedWidget")
         self.stackedWidget.setGeometry(QRect(0, 0, 771, 551))
         self.stackedWidget.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
-        self.page = QWidget()
-        self.page.setObjectName(u"page")
-        self.Face_listWidget = QListWidget(self.page)
+        self.page_0 = QWidget()
+        self.page_0.setObjectName(u"page_0")
+        self.groupBox_page_0 = QGroupBox(self.page_0)
+        self.groupBox_page_0.setObjectName(u"groupBox_page_0")
+        self.groupBox_page_0.setGeometry(QRect(230, 440, 320, 58))
+        self.label_page_0 = QLabel(self.groupBox_page_0)
+        self.label_page_0.setObjectName(u"label_page_0")
+        self.label_page_0.setGeometry(QRect(110, 7, 100, 42))
+        self.label_page_0.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.btn_pre_0 = QPushButton(self.groupBox_page_0)
+        self.btn_pre_0.setObjectName(u"btn_pre_0")
+        self.btn_pre_0.setGeometry(QRect(10, 8, 95, 40))
+        self.btn_next_0 = QPushButton(self.groupBox_page_0)
+        self.btn_next_0.setObjectName(u"btn_next_0")
+        self.btn_next_0.setGeometry(QRect(215, 8, 95, 40))
+        self.face_label = QLabel(self.page_0)
+        self.face_label.setObjectName(u"face_label")
+        self.face_label.setGeometry(QRect(90, 50, 551, 331))
+        self.stackedWidget.addWidget(self.page_0)
+        self.page_1 = QWidget()
+        self.page_1.setObjectName(u"page_1")
+        self.Face_listWidget = QListWidget(self.page_1)
         self.Face_listWidget.setObjectName(u"Face_listWidget")
         self.Face_listWidget.setGeometry(QRect(540, 180, 221, 261))
-        self.battery_label = QLabel(self.page)
+        self.battery_label = QLabel(self.page_1)
         self.battery_label.setObjectName(u"battery_label")
         self.battery_label.setGeometry(QRect(520, 20, 231, 42))
         self.battery_label.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
-        self.groupBox_page = QGroupBox(self.page)
-        self.groupBox_page.setObjectName(u"groupBox_page")
-        self.groupBox_page.setGeometry(QRect(205, 440, 320, 58))
-        self.label_page = QLabel(self.groupBox_page)
-        self.label_page.setObjectName(u"label_page")
-        self.label_page.setGeometry(QRect(110, 7, 100, 42))
-        self.label_page.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.btn_pre = QPushButton(self.groupBox_page)
-        self.btn_pre.setObjectName(u"btn_pre")
-        self.btn_pre.setGeometry(QRect(10, 8, 95, 40))
-        self.btn_next = QPushButton(self.groupBox_page)
-        self.btn_next.setObjectName(u"btn_next")
-        self.btn_next.setGeometry(QRect(215, 8, 95, 40))
-        self.groupBox_face = QGroupBox(self.page)
+        self.groupBox_page_1 = QGroupBox(self.page_1)
+        self.groupBox_page_1.setObjectName(u"groupBox_page_1")
+        self.groupBox_page_1.setGeometry(QRect(205, 440, 320, 58))
+        self.label_page_1 = QLabel(self.groupBox_page_1)
+        self.label_page_1.setObjectName(u"label_page_1")
+        self.label_page_1.setGeometry(QRect(110, 7, 100, 42))
+        self.label_page_1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.btn_pre_1 = QPushButton(self.groupBox_page_1)
+        self.btn_pre_1.setObjectName(u"btn_pre_1")
+        self.btn_pre_1.setGeometry(QRect(10, 8, 95, 40))
+        self.btn_next_1 = QPushButton(self.groupBox_page_1)
+        self.btn_next_1.setObjectName(u"btn_next_1")
+        self.btn_next_1.setGeometry(QRect(215, 8, 95, 40))
+        self.groupBox_face = QGroupBox(self.page_1)
         self.groupBox_face.setObjectName(u"groupBox_face")
         self.groupBox_face.setGeometry(QRect(530, 80, 241, 91))
         self.Face_lineEdit = QLineEdit(self.groupBox_face)
@@ -302,10 +322,10 @@ class Ui_Form(object):
         self.btn_face = QPushButton(self.groupBox_face)
         self.btn_face.setObjectName(u"btn_face")
         self.btn_face.setGeometry(QRect(160, 20, 71, 51))
-        self.face_label = QLabel(self.page)
-        self.face_label.setObjectName(u"face_label")
-        self.face_label.setGeometry(QRect(270, 60, 211, 331))
-        self.stackedWidget.addWidget(self.page)
+        self.camera_label = QLabel(self.page_1)
+        self.camera_label.setObjectName(u"camera_label")
+        self.camera_label.setGeometry(QRect(80, 30, 401, 391))
+        self.stackedWidget.addWidget(self.page_1)
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
         self.tabWidget = QTabWidget(self.page_2)
@@ -412,15 +432,20 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.groupBox_page_0.setTitle("")
+        self.label_page_0.setText(QCoreApplication.translate("Form", u"0page", None))
+        self.btn_pre_0.setText(QCoreApplication.translate("Form", u"PREV", None))
+        self.btn_next_0.setText(QCoreApplication.translate("Form", u"NEXT", None))
+        self.face_label.setText(QCoreApplication.translate("Form", u"face_label", None))
         self.battery_label.setText(QCoreApplication.translate("Form", u"\ubc30\ud130\ub9ac \uc794\ub7c9 :", None))
-        self.groupBox_page.setTitle("")
-        self.label_page.setText(QCoreApplication.translate("Form", u"1page", None))
-        self.btn_pre.setText(QCoreApplication.translate("Form", u"PREV", None))
-        self.btn_next.setText(QCoreApplication.translate("Form", u"NEXT", None))
+        self.groupBox_page_1.setTitle("")
+        self.label_page_1.setText(QCoreApplication.translate("Form", u"1page", None))
+        self.btn_pre_1.setText(QCoreApplication.translate("Form", u"PREV", None))
+        self.btn_next_1.setText(QCoreApplication.translate("Form", u"NEXT", None))
         self.groupBox_face.setTitle("")
         self.Face_lineEdit.setText("")
         self.btn_face.setText(QCoreApplication.translate("Form", u"\ud655\uc778", None))
-        self.face_label.setText(QCoreApplication.translate("Form", u"TextLabel", None))
+        self.camera_label.setText(QCoreApplication.translate("Form", u"camera_label", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Form", u"STATUS", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Form", u"CONTROL", None))
         self.groupBox_page_2.setTitle("")
